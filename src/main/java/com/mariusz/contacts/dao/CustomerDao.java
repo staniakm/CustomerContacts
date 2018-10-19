22f347e27d92ec375364cbfbfe643d93c7919b80
Mariusz <!Shadow83>
2018-10-19 19:56:07 +0200

    change access modifier for interface methods

--- src/main/java/com/mariusz/contacts/dao/CustomerDao.java
+++ src/main/java/com/mariusz/contacts/dao/CustomerDao.java
@@ -2,17 +2,16 @@ package com.mariusz.contacts.dao;
 import com.mariusz.contacts.entity.Customer;
 
 import java.util.List;
-import javax.sql.DataSource;
 
 
 public interface CustomerDao {
 
-    public void create(String name, String surname, String age);
+    void create(String name, String surname, String age);
 
-    public Customer getCustomer(Long id);
+    Customer getCustomer(Long id);
 
-    public List<Customer> listCustomers();
+    List<Customer> listCustomers();
 
-    public void delete(Long id);
+    void delete(Long id);
 
 }
