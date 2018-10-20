efe1073e63e35360fa6e36906c2aebdbcaf37ef6
Mariusz <!Shadow83>
2018-10-20 15:49:00 +0200

    add contact controller

---
+++ src/main/java/com/mariusz/contacts/controller/ContactController.java
@@ -0,0 +1,9 @@
+package com.mariusz.contacts.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/contacts")
+public class ContactController {
+}
