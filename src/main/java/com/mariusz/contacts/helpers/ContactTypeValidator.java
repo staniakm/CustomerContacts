9808b86efa2adf80bdd2cbd28b6e69bd83f1cd5c
Mariusz <!Shadow83>
2018-10-20 15:23:51 +0200

    change validator to static

--- src/main/java/com/mariusz/contacts/helpers/ContactTypeValidator.java
+++ src/main/java/com/mariusz/contacts/helpers/ContactTypeValidator.java
@@ -6,9 +6,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
-public class ContactVaidator {
+public class ContactTypeValidator {
 
-    public int validateContactType(String contact){
+    public static int validate(String contact){
         if (isPhoneNumber(contact)){
             return 2;
         }else if (isValidEmail(contact)){
@@ -19,20 +19,18 @@ public class ContactVaidator {
         return 0;
     }
 
-    private boolean isValidJabber(String contact) {
+    private static boolean isValidJabber(String contact) {
         return contact.contains("jbr");
     }
 
-    private boolean isPhoneNumber(String contact){
+    private static boolean isPhoneNumber(String contact){
         String isbnPattern = "([^\\d])+";
         if (contact!=null)
             return contact.replaceAll(isbnPattern,"").length()==9;
         return false;
     }
 
-
-
-    private boolean isValidEmail(String contact) {
+    private static boolean isValidEmail(String contact) {
         Pattern email_pattern =
                 Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
         Matcher matcher = email_pattern.matcher(contact);
